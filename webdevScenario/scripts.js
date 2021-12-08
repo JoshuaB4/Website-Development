be6d7516ed7332416ab016a4381a5ee14887ab1c
JoshuaB4 <90382832+JoshuaB4@users.noreply.github.com>
2021-12-08 15:31:13 +0000

    New stuff

---
+++ webdevScenario/scripts.js
@@ -0,0 +1,8 @@
+function alertBox() {
+    alert("Your application has been submitted.")
+}
+
+function popup() {
+    var popup = document.getElementById("confirmPopup");
+    popup.classList.toggle("show");
+  }
